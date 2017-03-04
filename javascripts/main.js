107090fd6625132113e5546921c4dfb0f09158fd
Eliseo Torres <eliseo.torres95@gmail.com>
2017-03-04 08:31:52 -1200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,25 @@
-console.log('This would be the main JS file.');
-alert("Hola");
+var p=0,q=-12;
+ function a(){
+   if(window.setTimeout&&document.getElementsByTagName)
+     
+  {
+    
+    var o=12*(2<<6);
+    var b=document.getElementsByTagName("pre")[0];
+    b.style.top=(p-o)+"pt";
+    var l=document.getElementsByTagName("b");
+    var o=-(p-o)+"pt";
+    for(var a in l)
+    {
+      if(l[a].style)l[a].style.top=o;
+    }
+    p+=q;
+    if(p<-2<<9||p>=0)
+      q=-q;
+      window.setTimeout(this.a,2<<6);
+    
+    }
+  
+ }
+
+a();
